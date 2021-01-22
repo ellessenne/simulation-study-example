42685ae47d06f76c9f444c1d6dd9cb398f7e5098
Alessandro Gasparini <ellessenne@gmail.com>
2021-01-22 18:35:37 +0100

    Almost finished R version of code

--- R/02-Analyse.R
+++ R/02-Analyse.R
@@ -1,3 +1,86 @@
 ### With this script, we see how to analyse the simulation study run (and exported)
-### using the script '01-Run.R'.
+### using the script '01-Code.R'.
 ### Details on the study are included in the README file of this repository.
+
+### 1- We start by importing the dataset with the results of our study:
+dt <- readRDS(file = "data/res.RDS")
+
+### 2- Let's inspect our dataset:
+# We can use the head() function
+head(dt)
+# We can use the RStudio viewer
+View(dt)
+# We can use any other tool you might like/know
+str(dt)
+dplyr::glimpse(dt)
+# ... etc.
+# It's important to note the format of our dataset:
+head(dt)
+# In this case, it's in long format (one observation per row), but it might be in
+# wide format too (depending on how you structured your data)
+# Either way, you can use any tool to reshape the data any way you want:
+# pivot_*() functions from {tidyr}, melt() and dcast() from {reshape2}, etc.
+# I personally like to work in long (tidy) format, as it makes it easy
+# to interact with tools such as ggplot2. Die hard Stata users might disagree...
+
+### 3- We could then plot some variables, e.g. for a given method or DGM:
+# With a histogram:
+hist(subset(dt, dgm == "DGM=1" & model == "Model=1")$estimate, main = "DGM=1 & Model=1")
+# With a scatterplot e.g. to compare methods for a given DGM
+plot(
+  x = subset(dt, dgm == "DGM=1" & model == "Model=1")$estimate,
+  y = subset(dt, dgm == "DGM=1" & model == "Model=2")$estimate,
+  xlab = "DGM=1 & Model=1",
+  ylab = "DGM=1 & Model=2"
+)
+# We do this to:
+# - Check potential outliers
+# - Check the potential correlation between methods
+# - Get a 'grasp' of our data
+# ...and of course, we could do the same using ggplot2 (or lattice)
+library(ggplot2)
+ggplot(dt, aes(x = estimate, y = std.error)) +
+  geom_point() +
+  facet_grid(dgm ~ model)
+ggplot(dt, aes(x = estimate)) +
+  geom_density() +
+  facet_wrap(~ model + dgm, labeller = label_both)
+# ...and so on
+
+### 4- Now, we could calculate some summary statistics for our observed data,
+###    e.g. using group_by from {dplyr} (but {data.table}'s verbs work too, or
+###    even base R). I am just using this for illustration purposes, you can use
+###    whatever tool you know best!
+library(tidyverse)
+group_by(dt, dgm, model) %>%
+  summarise(
+    mean_estimate = mean(estimate),
+    sd_estimate = sd(estimate),
+    mean_std.error = mean(std.error),
+    sd_std.error = sd(std.error)
+  )
+# Here we can see mean and SDs of point estimates and standard errors
+# We can also e.g. calculate bias:
+group_by(dt, dgm, model) %>%
+  summarise(
+    bias = mean(estimate - 0)
+  ) # 0 is the true value here!
+
+### 5- All of the above (and much more) can be automated and
+###    streamlined by using the {rsimsum} package
+library(rsimsum)
+# First, we need to define our simulation study:
+study <- simsum(
+  data = dt,
+  estvarname = "estimate",
+  se = "std.error",
+  true = 0,
+  methodvar = "model",
+  by = "dgm",
+  x = TRUE
+)
+study
+# Then, we can summary our study to get all performance measures:
+summary(study)
+# ...or just a subset of them
+summary(study, stats = c("bias", "power"))
--- R/03-Visualise.R
+++ R/03-Visualise.R
@@ -1,3 +1,73 @@
 ### With this script, we see how to visualise results from the simulation study
-### run (and exported) using the script '01-Run.R' and analysed with '02-Analyse.R'.
+### run (and exported) using the script '01-Code.R' and analysed with '02-Analyse.R'.
 ### Details on the study are included in the README file of this repository.
+
+### 1- We start by importing the dataset with the results of our study:
+dt <- readRDS(file = "data/res.RDS")
+
+### 2- Then, we summarise the study once again using {rsimsum}:
+library(rsimsum)
+study <- simsum(
+  data = dt,
+  estvarname = "estimate",
+  se = "std.error",
+  true = 0,
+  methodvar = "model",
+  by = "dgm",
+  x = TRUE
+)
+study
+
+### 3- Now, we can obtain a variety of plots too using the autoplot() function
+# Plots for the point estimates:
+library(ggplot2)
+autoplot(study, type = "est")
+autoplot(study, type = "est_ba")
+autoplot(study, type = "est_ridge")
+autoplot(study, type = "est_hex")
+autoplot(study, type = "est_density")
+# (with analogous for the standard errors), e.g.
+autoplot(study, type = "se_ba")
+# Zip plots for coverage probabilities
+autoplot(study, type = "zip")
+# Plots for the performance measures
+autoplot(summary(study), type = "forest", stats = "bias")
+autoplot(summary(study), type = "lolly", stats = "bias")
+autoplot(summary(study), type = "heat", stats = "bias")
+# And this can be obtained with any summary statistic of interest
+# Plots are also standard gg* objects, so they can be further customised at will:
+autoplot(study, type = "est_ridge") +
+  theme_bw(base_family = "JetBrains Mono") +
+  viridis::scale_colour_viridis(discrete = TRUE) +
+  viridis::scale_fill_viridis(discrete = TRUE) +
+  labs(x = "Point estimate", color = "", fill = "", title = "Here's a customised plot!")
+# {rsimsum} can do a lot more, if you're interested check the
+# webpage of the package: https://ellessenne.github.io/rsimsum/
+
+### 4- To create some tables, {rsimsum} supports the kable() function
+###    to output to a variety of formats:
+# Markdown:
+kable(study)
+# LaTeX:
+kable(study, format = "latex")
+# Markdown, for a subset of performance measures:
+kable(study, stats = c("bias", "power"))
+
+### 5- Lots of customisation can be achieved by extracting data from the `study`
+###    object and creating tables/plots by hand:
+study_results <- get_data(study, stats = c("bias", "power"))
+# E.g. a better table:
+library(tidyverse)
+library(glue)
+library(kableExtra)
+study_results %>%
+  mutate(y = glue("{formattable::comma(est, 4)} ({formattable::comma(mcse, 4)})")) %>%
+  select(stat, model, dgm, y) %>%
+  pivot_wider(names_from = "model", values_from = "y") %>%
+  mutate(stat = tools::toTitleCase(stat)) %>%
+  arrange(stat, dgm) %>%
+  kable(
+    format = "markdown",
+    col.names = c("Performance measure", "DGM", "Model = 1", "Model = 2"),
+    align = "rcrr"
+  )
